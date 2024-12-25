ba4caed3ec3007e5af67ed256f2fdc82e14db3e9
Chang Oh <72629686+chang4oh@users.noreply.github.com>
2024-12-24 22:15:59 -0600

    use the encoding 'utf-8-sig' to ensure proper rendering of special characters

--- scrapetocsv.py
+++ scrapetocsv.py
@@ -1,35 +1,31 @@
-#IMPORT LIBRARIES
+# IMPORT LIBRARIES
 from bs4 import BeautifulSoup
 import requests
-
-#IMPORT CSV LIBRARY
 import csv
 
-#OPEN A NEW CSV FILE. IT CAN BE CALLED ANYTHING
-file = open('scraped_quotes.csv', 'w')
-#CREATE A VARIABLE FOR WRITING TO THE CSV
+# OPEN A NEW CSV FILE WITH UTF-8 ENCODING
+file = open('scraped_quotes.csv', 'w', encoding='utf-8-sig')
+# CREATE A VARIABLE FOR WRITING TO THE CSV
 writer = csv.writer(file)
 
-#CREATE THE HEADER ROW OF THE CSV
+# CREATE THE HEADER ROW OF THE CSV
 writer.writerow(['Quote', 'Author'])
 
-#REQUEST WEBPAGE AND STORE IT AS A VARIABLE
+# REQUEST WEBPAGE AND SET THE CORRECT ENCODING
 page_to_scrape = requests.get("http://quotes.toscrape.com")
-#USE BEAUTIFULSOUP TO PARSE THE HTML AND STORE IT AS A VARIABLE
+page_to_scrape.encoding = 'utf-8'
+
+# USE BEAUTIFULSOUP TO PARSE THE HTML
 soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
-#FIND ALL THE ITEMS IN THE PAGE WITH A CLASS ATTRIBUTE OF 'TEXT'
-#AND STORE THE LIST AS A VARIABLE
-quotes = soup.findAll('span', attrs={'class':'text'})
 
-#FIND ALL THE ITEMS IN THE PAGE WITH A CLASS ATTRIBUTE OF 'AUTHOR'
-#AND STORE THE LIST AS A VARIABLE
-authors = soup.findAll('small', attrs={"class":"author"})
+# FIND ALL THE QUOTES AND AUTHORS
+quotes = soup.findAll('span', attrs={'class': 'text'})
+authors = soup.findAll('small', attrs={'class': 'author'})
 
-#LOOP THROUGH BOTH LISTS USING THE 'ZIP' FUNCTION
-#AND PRINT AND FORMAT THE RESULTS
+# LOOP THROUGH BOTH LISTS AND WRITE TO THE CSV
 for quote, author in zip(quotes, authors):
-    print(quote.text + "-" + author.text)
-    #WRITE EACH ITEM AS A NEW ROW IN THE CSV
+    print(quote.text + " - " + author.text)
     writer.writerow([quote.text, author.text])
-#CLOSE THE CSV FILE
+
+# CLOSE THE CSV FILE
 file.close()
